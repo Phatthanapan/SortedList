0bc4b83198af5b9c3ce8583f9b141cfad4a1f0e9
Phatthanapan <phatthanapan@Gmail.com>
2024-11-11 11:14:23 +0700

    Lecture-86

---
+++ Lecture-86/Lecture-86/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lecture_86
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SortedList sortedList;
+        public MainWindow()
+        {
+            InitializeComponent();
+            sortedList = new SortedList();  
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            sortedList.Add(int.Parse(ketTxt.Text),valueTxt.Text);
+            ICollection collection = sortedList.Keys;
+            string show = "";
+            foreach (int key in collection)
+            {
+               show = show + " " + key+"="+sortedList[key].ToString()+"\n";
+
+            }
+            MessageBox.Show(show);
+        }
+    }
+}
